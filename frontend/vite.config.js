70ca91e9761123482dfae00ed41d2c0d24b1b544
miafig <mercedes.figueiredo999@gmail.com>
2026-02-18 18:44:41 +0000

    add back workflow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/commutement/",
   plugins: [react()],
+  build: {
+    outDir: "build",
+  },
   server: {
     port: 3000,
   },
