c31f6b024b50259702de487cacd2b90b87a6cd0a
Mia <mercedes.figueiredo999@gmail.com>
2026-04-30 16:30:55 +0100

    env name update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.DEVELOPING === "dev"
+export const apiUrl = process.env.NODE_ENV === "development"
   ? "http://127.0.0.1:5000/api/"
   : "https://miafig-commutement.hf.space/api/";
