f2c31db8de32914ac71d58f35760e627208980f0
Mia <mercedes.figueiredo999@gmail.com>
2026-04-30 16:18:22 +0100

    update api url

--- frontend/src/pages/Train.jsx
+++ frontend/src/pages/Train.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-
-const apiUrl = "http://127.0.0.1:5000/api/" // "https://miafig-commutement.hf.space/api/";
+import { apiUrl } from "../config";
 
 const initTrainState = {
   epochs: 10,
